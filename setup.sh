f19d20cfe8c54ec5f429d96d8e0a698875efd517
initialpuppet <31685362+initialpuppet@users.noreply.github.com>
2017-11-05 17:35:29 +0200

    Extra software after initial test, firefox developer script

--- setup.sh
+++ setup.sh
@@ -4,12 +4,17 @@ set -x
 sudo apt update --assume-yes
 sudo apt upgrade --assume-yes
 
+# From Snap
+# done manually for now
+#sudo snap install keepassxc
+
 # From Ubuntu default PPA
+sudo apt install --assume-yes curl
+sudo apt install --assume-yes vim
 sudo apt install --assume-yes chromium-browser
-sudo apt install --assume-yes git
+sudo apt install --assume-yes git-all
 sudo apt install --assume-yes i3
 sudo apt install --assume-yes ldnsutils
-sudo apt install --assume-yes mtr-tiny
 sudo apt install --assume-yes nethogs
 sudo apt install --assume-yes nmap
 sudo apt install --assume-yes pmount
@@ -17,23 +22,20 @@ sudo apt install --assume-yes smplayer
 sudo apt install --assume-yes vlc
 sudo apt install --assume-yes weechat
 sudo apt install --assume-yes xautolock
-sudo apt install --assume-yes nodejs-legacy
+sudo apt install --assume-yes nodejs
 sudo apt install --assume-yes npm
 
 # Requires Microsoft PPA
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
-sudo apt install code # or code-insiders
+sudo apt --assume-yes update
+sudo apt --assume-yes install code # or code-insiders
 
 # Golang
 #sudo add-apt-repository ppa:gophers/archive
 #sudo apt update
 #sudo apt-get install golang-1.8-go
-
-# From Snap
-sudo snap install keepassxc
 
 # From NPM
 sudo npm install -g gulp
---
+++ userscripts/fxd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/opt/firefox/firefox -p developer -no-remote
