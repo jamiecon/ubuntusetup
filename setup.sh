ded19de3bddf6512934c69be50a9f4f55091961f
initialpuppet <31685362+initialpuppet@users.noreply.github.com>
2018-02-07 10:23:16 +0200

    stuff and updates and stuff

--- setup.sh
+++ setup.sh
@@ -2,36 +2,41 @@
 set -x
 
 # From Snap
-sudo snap install keepassxc
+#sudo snap install keepassxc
 
 sudo apt update --assume-yes
 sudo apt upgrade --assume-yes
 
-sudo apt install --assume-yes gimp
 sudo apt install --assume-yes i3
+
 sudo apt install --assume-yes ldnsutils
 sudo apt install --assume-yes whois
 sudo apt install --assume-yes nethogs
 sudo apt install --assume-yes nmap
 sudo apt install --assume-yes pmount
-sudo apt install --assume-yes smplayer
-sudo apt install --assume-yes vlc
-sudo apt install --assume-yes weechat
 sudo apt install --assume-yes xautolock
 sudo apt install --assume-yes numlockx
-sudo apt install --assume-yes qbittorrent
-sudo apt install --assume-yes nodejs
-sudo apt install --assume-yes npm
 sudo apt install --assume-yes tree 
 sudo apt install --assume-yes curl
 sudo apt install --assume-yes vim
+
+sudo apt install --assume-yes firefox
 sudo apt install --assume-yes chromium-browser
 
-# Only necessary for Ubuntu 16.07
-sudo apt install --assume-yes nodejs-legacy
+sudo apt install --assume-yes keepassx
+sudo apt install --assume-yes qbittorrent
+sudo apt install --assume-yes smplayer
+sudo apt install --assume-yes vlc
+sudo apt install --assume-yes weechat
+sudo apt install --assume-yes nodejs
+sudo apt install --assume-yes gimp
 
-# Git
+sudo apt install --assume-yes npm
 sudo apt install --assume-yes git
+sudo apt install --assume-yes python-pip
+sudo apt install --assume-yes python3.6-dev
+
+# Git config
 git config --global user.email "31685362+initialpuppet@users.noreply.github.com"
 git config --global user.name "initialpuppet"
 git config --global credential.helper "cache --timeout=86400"
@@ -46,15 +51,11 @@ Driver      \"intel\"
 Option      \"Backlight\"  \"intel_backlight\"
 EndSection" | sudo tee >> /etc/X11/xorg.conf
 
-# Disable X power management, i3lock can handle it
-xset dpms 0 0 0
-
-
 # For the Pixel Go game library
 #sudo apt install --assume-yes libgl1-mesa-dev
 #sudo apt install --assume-yes xorg-dev 
 
-# Requires Microsoft PPA
+# Visual Studio Code 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -70,22 +71,25 @@ sudo apt install --assume-yes code # or code-insiders
 sudo npm install -g gulp
 
 # Python packages
+sudo pip install virtualenv
 sudo pip install pylint
 
 # Firefox Dev Edition
 wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-GB" -O fx.tar.bz2
 tar -xvjf fx.tar.bz2
 sudo mv firefox /opt/firefoxdeveloper
+rm fx.tar.bz2
 
 # Scripts to go in the path
 sudo cp userscripts/* /usr/local/bin/
+sudo chmod a+x /usr/local/bin/*
 
 # i3 Config
-git clone https://initialpuppet@gitlab.com/initialpuppet/i3config.git ~/.config/i3
+#git clone https://initialpuppet@gitlab.com/initialpuppet/i3config.git ~/.config/i3
 
 # Create .ssh directory and configuration file
 mkdir ~/.ssh
 echo "#IdentityFile ~/.ssh/id_rsa
 # ... and so on" | tee >> ~/.ssh/config
 echo "Now copy your private keys into the .ssh directory and add the filenames to the config file."
-chomd 600 ~/.ssh/*
+chmod 600 ~/.ssh/*
